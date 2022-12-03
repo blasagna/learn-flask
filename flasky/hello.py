32a012610e9ff5fb6fb24e5cc6a3015a9f06610b
Bob DiMaiolo <bdimaiolo@gmail.com>
2022-12-02 20:18:35 -0500

    request context

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,3 +17,10 @@ def user(name):
 @app.route("/id/<int:uid>")
 def id(uid):
     return "<h1>Hello, ID {}!</h1>".format(uid)
+
+# Flask uses contexts to temporarily make certain objects globally accessible, like request.
+# The request object could be sent as an argument, but that would require every single view function in the application to have an extra parameter.
+@app.route('/user-agent')
+def user_agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
